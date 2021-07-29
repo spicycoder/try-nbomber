11a7276d2620ede772edff68e380cd6e6e801565
SpicyCoder <spk2naveen@hotmail.com>
2021-07-30 00:17:18 +0530

    Load testing with NBomber

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# try-nbomber
-Try NBomber for Load testing
+# Try [NBomber](https://github.com/PragmaticFlow/NBomber)
+
+Try Performance Testing with [NBomber](https://github.com/PragmaticFlow/NBomber)
+
+---
+
+## Launch API
+
+To launch the API, navigate to `src/WeatherAPI` & use PowerShell / Bash to run following command
+
+```ps1
+dotnet run -c Release
+```
+
+## Run Performance Tests
+
+To run `Performance Tests`, open another instance of PowerShell / Bash & run, navigate to `tests/WeatherAPI.LoadTests` following command
+
+```ps1
+dotnet run -c Release
+```
+
+---
+
+## Reports
+
+Reports will be generated at `tests/WeatherAPI.LoadTests/reports`
+
+Open the HTML file, for report with visualizations
+
+---
+
+## Realtime Reports
+
+`WIP`
+
+---
---
+++ tests/WeatherAPI.LoadTests/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NBomber.Contracts;
+using NBomber.CSharp;
+using NBomber.Plugins.Http.CSharp;
+using NBomber.Plugins.Network.Ping;
+
+namespace WeatherAPI.LoadTests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var forecastScenario = ForecastTest();
+            var historyScenario = HistoryTest();
+
+            var pingPluginConfig = PingPluginConfig.CreateDefault(new[] { "forecast", "history" });
+            var pingPlugin = new PingPlugin(pingPluginConfig);
+
+            NBomberRunner
+                .RegisterScenarios(forecastScenario, historyScenario)
+                .WithWorkerPlugins(pingPlugin)
+                .Run();
+        }
+
+        public static Scenario ForecastTest()
+        {
+            var step = Step.Create(
+                "forecast",
+                HttpClientFactory.Create(),
+                context =>
+                {
+                    var request = Http.CreateRequest("GET", "https://localhost:5001/WeatherForecast/forecast")
+                        .WithHeader("Accept", "application/json");
+
+                    return Http.Send(request, context);
+                });
+
+            var scenario = ScenarioBuilder
+                .CreateScenario("get_forecast", step)
+                .WithLoadSimulations(LoadSimulation.NewInjectPerSec(100, TimeSpan.FromSeconds(60)));
+
+            return scenario;
+        }
+
+        public static Scenario HistoryTest()
+        {
+            var step = Step.Create(
+                "history",
+                HttpClientFactory.Create(),
+                context =>
+                {
+                    var request = Http.CreateRequest("GET", "https://localhost:5001/WeatherForecast/history")
+                        .WithHeader("Accept", "application/json");
+
+                    return Http.Send(request, context);
+                });
+
+            var scenario = ScenarioBuilder
+                .CreateScenario("get_history", step)
+                .WithLoadSimulations(LoadSimulation.NewInjectPerSec(100, TimeSpan.FromSeconds(60)));
+
+            return scenario;
+        }
+    }
+}
